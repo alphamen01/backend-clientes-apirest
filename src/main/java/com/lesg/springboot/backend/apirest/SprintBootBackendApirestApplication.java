d81b24cd4c7d90ae8f5e9fbb99484fefe6843d5e
alphamen01 <lesg.2018@hotmail.com>
2020-02-10 23:56:59 -0500

    backend

---
+++ src/main/java/com/lesg/springboot/backend/apirest/SprintBootBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.lesg.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootBackendApirestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lesg/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.lesg.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.lesg.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao  extends CrudRepository<Cliente, Long>{
+
+}
